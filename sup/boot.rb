474af84780b2c825ddaf09992fee6155bfdc6d2e
Ignacio Baixas <iobaixas@gmail.com>
2015-07-01 16:29:21 -0300

    fix(boot): fixes app path expansion

--- sup/boot.rb
+++ sup/boot.rb
@@ -6,7 +6,7 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __FILE__)
 Bundler.setup
 
 # Setup code autoloading
-APP_PATH = File.expand_path('../', __FILE__)
+APP_PATH = File.expand_path('../../', __FILE__)
 
 require 'active_support/dependencies'
 ActiveSupport::Dependencies.autoload_paths += Dir.glob File.join(APP_PATH, 'src')
